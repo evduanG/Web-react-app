9c4e178ecb45b9e4fe8e86d27a0a969d2d896d76
Eviatar Duany <88398626+evduanG@users.noreply.github.com>
2022-07-19 14:36:37 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
