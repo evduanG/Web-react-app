c878264e73353064b5aa7c509d07c44197237ca5
Eviatar Duany <88398626+evduanG@users.noreply.github.com>
2022-08-03 23:00:57 +0300

    Update CocktailProductPageComp.js

--- client/src/components/CocktailProductPageComp/CocktailProductPageComp.js
+++ client/src/components/CocktailProductPageComp/CocktailProductPageComp.js
@@ -3,37 +3,44 @@ import YoutubeEmbed from "../../components/YoutubeEmbed/YoutubeEmbed";
 import "./CocktailProductPageComp.css";
 const CocktailProductPageComp = ({ drink }) => {
   return (
-    <div className="cocktailS">
-      <h3>{drink.title}</h3>
+    <div className="container">
       <div className="cocktailS-img">
         <img src={drink.image} alt="cocktail" />
       </div>
+      <h1>{drink.title}</h1>
       <div className="cocktail-info">
-        <div className="cocktail-info">
-          <h4>{drink.strCategory}</h4>
-          <div className="cocktail-Ingredient">
-            <h4>Ingredients</h4>
-            <ul>
-              {drink.Ingredients.map((ingredient) => {
-                return (
-                  <li key={`${ingredient.type}`}>
-                    {ingredient.type} {ingredient.quantity}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="cocktail-info">
-            <h4> how to mack</h4>
-            <ul>
-              {drink.preparationStages.map((stage) => {
-                return <li key={`${stage}`}>{stage.step}</li>;
-              })}
-            </ul>
-          </div>
+        <div className="cocktail-Ingredient">
+          <h2>Ingredients</h2>
+
+          <dl>
+            {drink.Ingredients.map((ingredient) => {
+              return (
+                <>
+                  <dt key={`${ingredient.type}`}> {ingredient.type}</dt>
+                  <dd> {ingredient.quantity}</dd>
+                  <br />
+                </>
+              );
+            })}
+          </dl>
         </div>
+        <div className="cocktail-Ingredient">
+          <h2> how to mack</h2>
+          <ul>
+            {drink.preparationStages.map((stage) => {
+              return (
+                <>
+                  {" "}
+                  <li key={`${stage}`}>{stage.step}</li> <br />
+                </>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="cocktail-info">
         <h2>glass : {drink.strGlass}</h2>
-        <h2>ingredients :</h2>
+        <br />
       </div>
       <YoutubeEmbed videoId={drink.strVideo} />
     </div>
